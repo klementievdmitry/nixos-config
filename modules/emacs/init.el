ec8ef545588b169da6e238c569e8bb4770198546
klvdmy <klvdmy@proton.me>
2023-03-28 22:54:49 +0500

    Added vanilla Emacs configuration

---
+++ modules/emacs/init.el
@@ -0,0 +1,136 @@
+;;; init.el --- Initial configuration file -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Klementiev Dmitry
+;;
+;; Author: Klementiev Dmitry <klementievdmitry@gmail.com>
+;; Maintainer: Klementiev Dmitry <klementievdmitry@gmail.com>
+;; Created: марта 28, 2023
+;; Modified: марта 28, 2023
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/klvdmy/init
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;; Just initial config file
+;;
+;;
+;;; Code:
+
+(provide 'init)
+
+;; No startup message
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)    ; Disable visible scrollbar
+(tool-bar-mode -1)      ; Disable toolbar
+(tooltip-mode -1)       ; Disable tooltips
+(set-fringe-mode 10)    ; Give some breathing room
+(menu-bar-mode -1)      ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+;; Set the font size
+(set-face-attribute 'default nil :font "JetBrains Mono" :height 130)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; All the icons
+(use-package all-the-icons)
+
+;; Set custom theme
+(use-package kaolin-themes
+  :config
+  (load-theme 'kaolin-eclipse)
+  (kaolin-treemacs-theme))
+
+;; Show line and column numbers
+(column-number-mode)                 ; Show column number in modeline
+(global-display-line-numbers-mode t) ; Show line numbers
+
+;; Hide line numbers in org/term/eshell modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Command log mode
+(use-package command-log-mode
+  :config
+  (global-command-log-mode 1))
+
+;; Use Ivy and Counsel for completions
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package counsel
+  :custom
+  (ivy-initial-inputs-alist nil) ; Don't start searches with ^
+  :config
+  (counsel-mode 1))
+
+;; Doom modeline
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom
+  (doom-modeline-height 30))
+
+;; Rainbow delimiters
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Ivy rich
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+;; Helpful - A better emacs *help* buffer
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;;; init.el ends here
