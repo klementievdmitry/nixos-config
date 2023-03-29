1a632d2730c026af59712a2ccdeeab1c106eeadb
klvdmy <klvdmy@proton.me>
2023-03-29 21:18:30 +0500

    Added Emacs projectile package

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -204,4 +204,19 @@
   (klvdmy/leader-keys
     "ts" '(hydra-text-scale/body :which-key "scale text")))
 
+;; Projectile
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom
+  (projectile-completion-system 'ivy)
+  :bind-keymap ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Projects/Code")
+    (setq projectile-project-search-path '("~/Projects/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
 ;;; init.el ends here
