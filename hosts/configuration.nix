0ae72bab22f8de4dcb29ee8ed8fde25eec8983af
klvdmy <klvdmy@proton.me>
2023-03-30 17:34:28 +0500

    Changed pipewire config

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -1,5 +1,7 @@
 { config, lib, pkgs, user, inputs, home-manager, system, doom-emacs, ... }:
 
+let
+in
 {
   networking.networkmanager.enable = true;
 
@@ -58,6 +60,7 @@
       pciutils
       usbutils
       wget
+      rnnoise-plugin
     ];
 
     etc = {
@@ -90,6 +93,82 @@
       };
       pulse.enable = true;
       jack.enable = true;
+
+      config.pipewire = {
+      	"context.properties" = {
+	  "log.level" = 0;
+	};
+
+	"context.spa-libs" = {
+	  "audio.convert.*" = "audio/libspa-audioconvert";
+	  "support.*" = "support/libspa-support";
+	};
+
+        "context.modules" = [
+	{
+	  name = "libpipewire-module-rtkit";
+          args = {
+            "nice.level" = -11;
+            "rt.prio" = 88;
+            "rt.time.soft" = 200000;
+            "rt.time.hard" = 200000;
+          };
+          flags = [ "ifexists" "nofail" ];
+        }
+        { name = "libpipewire-module-protocol-native"; }
+        { name = "libpipewire-module-profiler"; }
+        { name = "libpipewire-module-metadata"; }
+        { name = "libpipewire-module-spa-device-factory"; }
+        { name = "libpipewire-module-spa-node-factory"; }
+        { name = "libpipewire-module-client-node"; }
+        { name = "libpipewire-module-client-device"; }
+        {
+          name = "libpipewire-module-portal";
+          flags = [ "ifexists" "nofail" ];
+        }
+        {
+          name = "libpipewire-module-access";
+          args = {};
+        }
+        { name = "libpipewire-module-adapter"; }
+        { name = "libpipewire-module-link-factory"; }
+        { name = "libpipewire-module-session-manager"; }
+	{
+	  name = "libpipewire-module-filter-chain";
+	  args = {
+	    "node.description" = "Noise Canceling source";
+	    "media.name" = "Noise Canceling source";
+	    "filter.graph" = {
+	      "nodes" = [
+	        {
+		  "type" = "ladspa";
+		  "name" = "rnnoise";
+		  "plugin" = "/path/to/librnnoise_ladspa.so";
+		  "label" = "noise_suppressor_mono";
+		  "control" = {
+		    "VAD Threshold (%)" = 50.0;
+		    "VAD Grace Period (ms)" = 200;
+		    "Retroactive VAD Grace (ms)" = 0;
+		  };
+		}
+	      ];
+	    };
+
+	    "capture.props" = {
+	      "node.name" = "capture.rnnoise_source";
+	      "node.passive" = true;
+	      "audio.rate" = 48000;
+	    };
+
+	    "playback.props" = {
+	      "node.name" = "rnnoise_source";
+	      "media.class" = "Audio/Source";
+	      "audio.rate" = 48000;
+	    };
+	  };
+	}
+	];
+      };
     };
 
     openssh = {
