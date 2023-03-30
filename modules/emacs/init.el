0425047d181d14810a9b00b2f564a9edab72e9c8
klvdmy <klvdmy@proton.me>
2023-03-30 22:33:27 +0500

    Many changes in Emacs conf
    * Enabled electric-pair-mode and show-paren-mode
    * Added lsp-mode and company-mode
    * Added nix-mode
    * Added typescript-mode

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -51,6 +51,12 @@
     (add-hook 'after-make-frame-functions #'klvdmy/load-face-attributes-to-frame)
   (klvdmy/load-face-attributes))
 
+;; Enable electric pair mode
+(electric-pair-mode 1)
+
+;; Enable show paren mode
+(show-paren-mode 1)
+
 ;; Initialize package sources
 (require 'package)
 
@@ -295,5 +301,58 @@
   :hook (org-mode . org-bullets-mode)
   :custom
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+;; LSP mode
+(defun klvdmy/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . klvdmy/lsp-mode-setup)
+  :custom
+  (lsp-keymap-prefix "C-c l") ;; Or "C-l", "s-l"
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode))
+
+;; Company mode - Emacs autocompletion
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+	      ("<tab>" . company-complete-selection)
+	      :map lsp-mode-map
+	      ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+;; Nix mode
+(use-package nix-mode
+  :mode "\\.nix\\'"
+  :hook (nix-mode . lsp-deferred))
+(use-package nix-drv-mode
+  :ensure nix-mode
+  :mode "\\.drv\\'"
+  :hook (nix-drv-mode . lsp-deferred))
+(use-package nix-shell
+  :ensure nix-mode
+  :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
+(use-package nix-repl
+  :ensure nix-mode
+  :commands (nix-repl))
+
+;; TypeScript mode
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :custom
+  (typescript-indent-level 2))
 
 ;;; init.el ends here
