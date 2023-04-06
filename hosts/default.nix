312d70cfeb6f0d91713f9a817f122f9f61984717
klementievdmitry <klementievdmitry@gmail.com>
2023-04-06 20:28:06 +0500

    Added new desktop host

--- hosts/default.nix
+++ hosts/default.nix
@@ -43,4 +43,37 @@ in
       }
     ];
   };
+  
+  desktop = lib.nixosSystem {
+    inherit system;
+    specialArgs = {
+        inherit inputs user system hyprland hyprpaper wired;
+      host = {
+        hostName = "desktop";
+      };
+    };
+    modules = [
+      hyprland.nixosModules.default
+      ./desktop
+      ./configuration.nix
+
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = {
+          inherit user;
+          host = {
+            hostName = "desktop";
+          };
+        };
+        home-manager.users.${user} = {
+          imports = [
+            hyprland.homeManagerModules.default
+            ./home.nix
+            ./desktop/home.nix
+          ];
+        };
+      }
+    ];
+  };
 }
---
+++ hosts/desktop/default.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, user, ... }:
+
+{
+  imports =
+    [ ( import ./hardware-configuration.nix ) ] ++ # Hardware conf
+    [ ( import ../../modules/programs/games.nix ) ] ++ # Games
+    [ ( import ../../modules/sway/default.nix ) ]; # Sway WM
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    initrd.kernelModules = [ "amdgpu" "k10temp" ];
+    kernelParams = [
+      # GPU Drivers
+      "radeon.si_support=0"
+      "amdgpu.si_support=1"
+      "radeon.cik_support=0"
+      "amdgpu.cik_support=1"
+      "amdgpu.dc=1"
+      "amdgpu.dpm=1"
+    ];
+
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 5;
+      };
+
+      timeout = 1;
+    };
+  };
+
+  # GPU Drivers
+  services = {
+    xserver = {
+      enable = true;
+      videoDrivers = [ "amdgpu" ];
+    };
+  };
+
+  # HIP for programs like Blender
+  systemd.tmpfiles.rules = [
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.hip}"
+  ];
+
+  hardware.opengl.extraPackages = with pkgs; [
+    rocm-opencl-icd
+    rocm-opencl-runtime
+  ];
+
+  hardware.opengl = {
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  security.polkit.enable = true;
+}
