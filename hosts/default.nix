3fb53f89c9d52081c2dddd068e3addc983fdd4ea
ryveti <klementievdmitry@gmail.com>
2023-05-13 19:45:12 +0000

    Some mistake fix

--- hosts/default.nix
+++ hosts/default.nix
@@ -45,4 +45,39 @@ in
       }
     ];
   };
+  
+  laptop = lib.nixosSystem {
+    inherit system;
+    specialArgs = {
+      # Configuration special args
+      inherit inputs user system state-version;
+
+      host = {
+        hostName = "laptop";
+      };
+    };
+    modules = [
+      ./laptop
+      ./configuration.nix
+
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = {
+          # Home-manager special args
+          inherit user state-version;
+
+          host = {
+            hostName = "laptop";
+          };
+        };
+        home-manager.users.${user} = {
+          imports = [
+            ./home.nix
+            ./laptop/home.nix
+          ];
+        };
+      }
+    ];
+  };
 }
