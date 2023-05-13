f701a0691a331fed241ba04b402ca507d0f2b521
Klementiev Dmitry <109908018+ryveti@users.noreply.github.com>
2023-05-13 19:20:24 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,28 +3,33 @@
 
   inputs =
     {
-      stable.url = "github:nixos/nixpkgs/nixos-22.11";
+      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.11";
 
       home-manager = {
         url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "stable";
+        inputs.nixpkgs.follows = "nixpkgs-stable";
       };
     };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, ... }:
-    let
-      # User X
-      user = "x";
-      
-      # Using variable for state version
-      state-version = "22.11";
-    in
-    {
-      nixosConfigurations = (
-        import ./hosts {
-          inherit (nixpkgs) lib;
-          inherit inputs nixpkgs user home-manager state-version;
-        }
-      );
-    };
+  outputs = inputs @ {
+    self,
+    nixpkgs-stable,
+    home-manager,
+    ...
+  }:
+  let
+    # User X
+    user = "x";
+    
+    # Using variable for state version
+    state-version = "22.11";
+  in
+  {
+    nixosConfigurations = (
+      import ./hosts {
+        inherit (nixpkgs-stable) lib;
+        inherit inputs nixpkgs-stable user home-manager state-version;
+      }
+    );
+  };
 }
