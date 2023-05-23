9a6264811d7f7f011a7d09ed07cfec177b629784
ryveti <klementievdmitry@gmail.com>
2023-05-23 20:27:16 +0500

    Minor gnome config changes

--- modules/gnome/home.nix
+++ modules/gnome/home.nix
@@ -26,11 +26,10 @@ with lib;
         "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
         "caffeine@patapon.info"
         "clipboard-indicator@tudmotu.com"
-        "arcmenu@arcmenu.com"
         "custom-hot-corners-extended@G-dH.github.com"
-        "dash-to-panel@jderose9.github.com"
         "openweather-extension@jenslody.de"
         "tiling-assistant@leleat-on-github"
+        "color-picker@tuberry"
       ];
     };
    
@@ -124,52 +123,6 @@ with lib;
       city = "52.7586837,52.28831670221607>Бузулук, Оренбургская область, Приволжский федеральный округ, Россия>0";
     };
 
-    "org/gnome/shell/extensions/dash-to-panel" = with hm.gvariant; {
-      animate-appicon-hover = true;
-      animate-appicon-hover-animation-convexity = mkArray (type.dictionaryEntryOf [type.string type.double]) [
-        (mkDictionaryEntry ["RIPPLE" 2.0])
-        (mkDictionaryEntry ["PLANK" 1.0])
-        (mkDictionaryEntry ["SIMPLE" 0.0])
-      ];
-      animate-appicon-hover-animation-extent = mkArray (type.dictionaryEntryOf [type.string type.int32]) [
-        (mkDictionaryEntry ["RIPPLE" 4])
-        (mkDictionaryEntry ["PLANK" 4])
-        (mkDictionaryEntry ["SIMPLE" 1])
-      ];
-      animate-appicon-hover-animation-travel = mkArray (type.dictionaryEntryOf [type.string type.double]) [
-        (mkDictionaryEntry ["SIMPLE" 0.29999999999999999])
-        (mkDictionaryEntry ["RIPPLE" 0.40000000000000002])
-        (mkDictionaryEntry ["PLANK" 0.0])
-      ];
-      animate-appicon-hover-animation-type = "SIMPLE";
-      appicon-margin = 8;
-      appicon-padding = 4;
-      dot-position = "BOTTOM";
-      dot-style-focused = "DASHES";
-      dot-style-unfocused = "DOTS";
-      group-apps = true;
-      leftbox-padding = 0;
-      leftbox-size = 0;
-      panel-anchors = ''{"0":"MIDDLE"}'';
-      panel-lengths = ''{"0":100}'';
-      panel-positions = ''{"0":"TOP"}'';
-      show-favorites = true;
-      show-favorites-all-monitors = true;
-      show-running-apps = true;
-      status-icon-padding = 2;
-      stockgs-force-hotcorner = false;
-      stockgs-keep-dash = false;
-      stockgs-keep-top-panel = false;
-      stockgs-panelbtn-click-only = false;
-      trans-bg-color = "#241f31";
-      trans-panel-opacity = 0.7;
-      trans-use-custom-bg = true;
-      trans-use-custom-opacity = true;
-      tray-padding = 4;
-      tray-size = 0;
-      window-preview-title-position = "TOP";
-    };
-
     "org/gnome/desktop/input-sources" = with hm.gvariant; {
       per-window = true;
       sources = mkArray (type.tupleOf [type.string type.string]) [
@@ -177,13 +130,6 @@ with lib;
         (mkTuple ["xkb" "ru"])
       ];
       xkb-options = mkArray type.string ["grp:win_space_toggle"];
-    };
-
-    "org/gnome/shell/extensions/arcmenu" = with hm.gvariant; {
-      enable-menu-hotkey = true;
-      enable-standlone-runner-menu = true;
-      runner-menu-hotkey-type = "Custom";
-      runner-menu-custom-hotkey = ["<Ctrl>space"];
     };
   };
 
@@ -195,11 +141,10 @@ with lib;
     gnomeExtensions.caffeine
     gnomeExtensions.clipboard-indicator
     gnomeExtensions.auto-move-windows
-    gnomeExtensions.arcmenu
-    gnomeExtensions.dash-to-panel
     gnomeExtensions.custom-hot-corners-extended
     gnomeExtensions.openweather
     gnomeExtensions.tiling-assistant
+    gnomeExtensions.color-picker
   ];
 }
 
